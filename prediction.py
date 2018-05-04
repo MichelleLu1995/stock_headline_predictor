a928bfe5795b7fa1a2a8374947f8501c1c9137e8
Kristen Wang <kristenwang@dyn-160-39-254-214.dyn.columbia.edu>
2018-05-03 23:53:41 -0400

    added query_news_articles function

--- prediction.py
+++ prediction.py
@@ -17,102 +17,140 @@ sources = 'bbc-news,the-verge'
 # domain
 
 def main():
-	# retrieve s&p 500 companies 
-	df = pd.read_csv('constituents_csv.csv')
-	companies = df['Name']
-	company_symb = {}
+    # retrieve s&p 500 companies 
+    df = pd.read_csv('constituents_csv.csv')
+    companies = df['Name']
+    company_symb = {}
 
 
-	# Iterate through companies
-	for company in companies:
+    # Iterate through companies
+    for company in companies:
 
-		# get company ticker
-		company_symb[company] = df[df['Name'] == company]['Symbol']
+        # get company ticker
+        company_symb[company] = df[df['Name'] == company]['Symbol']
 
-		# initialize the dataframe
-		df_current = initialize_dataframe(company_symb[company], start_date, end_date)
+        # initialize the dataframe
+        df_current = initialize_dataframe(company_symb[company], start_date, end_date)
 
-		# Query news articles
-		dict_current = query_news_articles(company, start_date, end_date, sources)
+        # Query news articles
+        trading_dates = df['Date']
+        dict_current = query_news_articles(company, start_date, end_date, trading_dates, sources)
 
-		# iterate through dates
-		for date in dict_current.keys():
-			# when you enter seniment into the dataframe, use the before date not after
-			average_sentiment_dict = calculate_sentiment(dict_current[date])
+        # iterate through dates
+        for date in dict_current.keys():
+            # when you enter seniment into the dataframe, use the before date not after
+            average_sentiment_dict = calculate_sentiment(dict_current[date])
 
-			# Plug this into df current
+            # Plug this into df current
 
-		# Regression
+        # Regression
 
 def calculate_sentiment(sentence_arr):
-	""" Returns the average sentiment of the array
-	Params:
-		sentence_arr(Array): Array of setences that we have to calculate
-		the sentiment of.
-	Returns:
-		sentiment (dictionary): Takes the average of all sentences
-		format of score is {'neg': 0.0, 'neu': 1.0, 'pos': 0.0, 'compound': 0.0}
-	"""
-	sia = SentimentIntensityAnalyzer()
-	df = df = pd.DataFrame(columns=['neg','neu','pos','compound'])
-	for sentence in sentence_arr:
-		sentiment = sia.polarity_scores(sentence)
-		df_sentiment = pd.DataFrame([sentiment], columns=sentiment.keys())
-		df = df.append(df_sentiment)
-	
-	avg_sentiment = dict(df.mean())	
-	return avg_sentiment 
+    """ Returns the average sentiment of the array
+    Params:
+        sentence_arr(Array): Array of setences that we have to calculate
+        the sentiment of.
+    Returns:
+        sentiment (dictionary): Takes the average of all sentences
+        format of score is {'neg': 0.0, 'neu': 1.0, 'pos': 0.0, 'compound': 0.0}
+    """
+    sia = SentimentIntensityAnalyzer()
+    df = df = pd.DataFrame(columns=['neg','neu','pos','compound'])
+    for sentence in sentence_arr:
+        sentiment = sia.polarity_scores(sentence)
+        df_sentiment = pd.DataFrame([sentiment], columns=sentiment.keys())
+        df = df.append(df_sentiment)
+    
+    avg_sentiment = dict(df.mean())    
+    return avg_sentiment 
 
 
 # return dataframe with price_t and price_t-1
 def initialize_dataframe(ticker, start_date, end_date):
-	""" Initializes a data frame for a certain ticker
-	Params:
-		ticker (String): Stock ticker to be analyzed
-		start_date (String): Start date in format of "2001-12-31"
-		end_date (String): End date in format of "2001-12-31"
-	Returns:
-		dataframe (pd.Dataframe): Dataframe with index 'Date' ,columns 'X_t' and 'X_t-1'
-	"""
+    """ Initializes a data frame for a certain ticker
+    Params:
+        ticker (String): Stock ticker to be analyzed
+        start_date (String): Start date in format of "2001-12-31"
+        end_date (String): End date in format of "2001-12-31"
+    Returns:
+        dataframe (pd.Dataframe): Dataframe with index 'Date' ,columns 'X_t' and 'X_t-1'
+    """
 
-	# Convert start_date and end_date into a string
-	start_date_day, start_date_hours = start_date.split('T')
-	start_date_arr = start_date_day.split('-')
-	start_date_string = datetime.date(int(start_date_arr[0]), int(start_date_arr[1]), int(start_date_arr[2]))
-	end_date_day, end_date_hours = end_date.split('T')
-	end_date_arr = end_date_day.split('-')
-	end_date_string = datetime.date(int(end_date_arr[0]), int(end_date_arr[1]), int(end_date_arr[2]))
+    # Convert start_date and end_date into a string
+    start_date_day, start_date_hours = start_date.split('T')
+    start_date_arr = start_date_day.split('-')
+    start_date_string = datetime.date(int(start_date_arr[0]), int(start_date_arr[1]), int(start_date_arr[2]))
+    end_date_day, end_date_hours = end_date.split('T')
+    end_date_arr = end_date_day.split('-')
+    end_date_string = datetime.date(int(end_date_arr[0]), int(end_date_arr[1]), int(end_date_arr[2]))
 
-	# Query quandl for data and make dataframe
-	dataframe = quandl.get('EOD/'+ticker, start_date=start_date_string, end_date=end_date_string)['Adj_Close']
-	dataframe = pd.Series.to_frame(dataframe)
+    # Query quandl for data and make dataframe
+    dataframe = quandl.get('EOD/'+ticker, start_date=start_date_string, end_date=end_date_string)['Adj_Close']
+    dataframe = pd.Series.to_frame(dataframe)
 
-	# Make columns of dataframe
-	dataframe.columns = ['X_t']
-	dataframe['X_t-1'] = dataframe['X_t'].shift(1)
+    # Make columns of dataframe
+    dataframe.columns = ['X_t']
+    dataframe['X_t-1'] = dataframe['X_t'].shift(1)
 
-	# Remove the first data point because of shift
-	dataframe = dataframe.iloc[1:]
-	return dataframe
+    # Remove the first data point because of shift
+    dataframe = dataframe.iloc[1:]
+    return dataframe
 
 
 # query the news articles for the entire time frame and split by dates 
-# returns dictionaries of headlines and dates 
-# parse the headlines by date
-# key is date and value is headlines 
-def query_news_articles(company, start_date, end_date, sources):
-	""" Queries news article for a certain time frame and split it by dates
-		Note that we split the date differently based on before and after 4pm EST.
-	Params:
-		company (String): Name of company
-		start_date (String): Start date in format of "2001-12-31"
-		end_date (String): End date in format of "2001-12-31"
-		sources (Array of Strings): Array of different news sources
-	Returns:
-		company_dic (dictionary): keys are date, values are array of headlines
-	"""
-	company_dict = {}
-	return company_dic
+# returns dictionary of key-value pairs where key is trading date in 
+# format YYYY-MM-DD and value is array of all headlines affecting the stock
+# price on key date
+def query_news_articles(company, start_date, end_date, trading_dates, sources):
+    """ Queries news article for a certain time frame and split it by dates
+        Note that
+    Params:
+        company (String): Name of company
+        start_date (String): Start date in format of "2001-12-31"
+        end_date (String): End date in format of "2001-12-31"
+         trading_dates (Array of Strings): Array of dates when the market was open
+                 dates in format of "2001-12-31"
+        sources (Array of Strings): Array of different news sources
+    Returns:
+        company_dic (dictionary): keys are date, values are array of headlines
+    """
+    company_dict = dict.fromkeys(trading_dates.date,[])
+    newsdata = newsapi.get_everything(q=company,
+                                      sources=sources,
+                                      from_param=start_date,
+                                      to=end_date,
+                                      language='en',
+                                      sort_by='relevancy',
+                                      page=2)
+    
+    articles = newsdata['articles']
+    for article in articles:
+        headline = article['title']
+        # description = article['description']
+        # format of date is 2018-04-13T00:46:59Z (UTC format)
+        publish_date = article['publishedAt'] 
+        # adjust date for trading day
+        publish_date, publish_time = publish_date.split('T')
+        date_arr = publish_date.split('-')
+        publish_datetime = datetime.date(int(date_arr[0]), int(date_arr[1]), int(date_arr[2]))
+        time_arr = publish_time[:-1].split(':')
+        # stock market closes at 4:00 PM EST; if article published after 
+        # 16:00:00+4:00:00 = 20:00:00 UTC headline affects next trading day;
+        # otherwise affects current trading day
+        trading_datetime = publish_datetime
+        if int(time_arr[0]) >= 20:
+            trading_datetime += datetime.timedelta(1)
+#        trading_date = (trading_datetime).isoformat()
+        
+        # if given trading_date invalid (ie if article published on Friday 
+        # after market close, Saturday, or Sunday before 4 pm est) push trading_date
+        # to the following Monday (ie first valid trading_date)
+        while trading_datetime not in trading_dates:
+            trading_datetime += datetime.timedelta(1)
+#            trading_date = (trading_datetime).isoformat()
+        company_dict[trading_datetime].append(headline)
+    return company_dict
 
-initialize_dataframe('MSFT', start_date, end_date)
+current_df = initialize_dataframe('MSFT', start_date, end_date)
+print(query_news_articles('Microsoft', start_date, end_date, current_df.index, sources))
 
